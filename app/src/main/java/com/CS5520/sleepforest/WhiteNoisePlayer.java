42c8a67085493be7db64662c02e9def2443f6d6d
miadong <dong.xinm@husky.neu.edu>
2020-04-13 01:30:12 -0400

    white noise ui

---
+++ app/src/main/java/com/CS5520/sleepforest/WhiteNoisePlayer.java
@@ -0,0 +1,61 @@
+package com.CS5520.sleepforest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+public class WhiteNoisePlayer extends AppCompatActivity {
+
+    Button playBtn;
+    SeekBar positionBar;
+    SeekBar volumnBar;
+    TextView elapsedTimeLabel;
+    TextView remainingTimeLabel;
+    MediaPlayer mp;
+    int totalTime;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_white_noise_player);
+
+        playBtn = (Button)findViewById(R.id.playBtn);
+        elapsedTimeLabel = (TextView)findViewById(R.id.elapsedTimeLabel);
+        remainingTimeLabel = (TextView)findViewById(R.id.remainingTimeLabel);
+
+        // media player
+        play();
+
+        positionBar = (SeekBar)findViewById(R.id.positionBar);
+        positionBar.setMax(totalTime );
+
+        volumnBar = (SeekBar)findViewById(R.id.volumnBar);
+
+    }
+
+    private void play() {
+        mp = MediaPlayer.create(this, R.raw.pn01);
+        mp.setLooping(true);
+        mp.seekTo(0);
+        mp.setVolume(0.5f, 0.5f);
+        totalTime = mp.getDuration();
+    }
+
+    public void playBtnClick(View view) {
+        if (!mp.isPlaying()) {
+            // stopping
+            mp.start();
+            playBtn.setBackgroundResource(R.drawable.stop);
+        }
+        else {
+            // playing
+            mp.stop();
+            playBtn.setBackgroundResource(R.drawable.play );
+        }
+    }
+}
