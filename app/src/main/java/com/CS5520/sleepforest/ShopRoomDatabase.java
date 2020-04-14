89d8115b52f0b7991a6aa1bf26891d6630ea08d3
chen <gu.che@husky.neu.edu>
2020-04-13 21:31:14 -0400

    add shopimg and database

---
+++ app/src/main/java/com/CS5520/sleepforest/ShopRoomDatabase.java
@@ -0,0 +1,28 @@
+package com.CS5520.sleepforest;
+
+
+import android.content.Context;
+
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+@Database(entities = {Shop.class}, version =1)
+public abstract class ShopRoomDatabase extends RoomDatabase {
+
+    public abstract ShopDao shopDao();
+    private static ShopRoomDatabase INSTANCE;
+
+    static ShopRoomDatabase getDatabase(final Context context){
+        if(INSTANCE == null){
+            synchronized (TimeRoomDatabase.class){
+                if(INSTANCE == null){
+                    INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
+                            ShopRoomDatabase.class,"shop_database").build();
+                }
+            }
+        }
+            return INSTANCE;
+        }
+}
