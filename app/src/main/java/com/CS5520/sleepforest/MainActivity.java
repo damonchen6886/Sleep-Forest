316b58f52251114c06e730e32e8f0c55ac7cf798
rouni.yin <yin.ro@husky.neu.edu>
2020-04-15 17:34:49 -0400

    overlap fixed

--- app/src/main/java/com/CS5520/sleepforest/MainActivity.java
+++ app/src/main/java/com/CS5520/sleepforest/MainActivity.java
@@ -61,7 +61,7 @@ public class MainActivity extends AppCompatActivity implements OnRegisterSuccess
         navigationView.setNavigationItemSelectedListener(this);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        mAppBarConfiguration = new AppBarConfiguration.Builder(R.id.nav_blank,
+        mAppBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow,
                 R.id.nav_tools)
                 .setDrawerLayout(drawer)
